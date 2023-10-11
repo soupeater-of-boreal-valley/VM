04fd6cf8ccb47a274847b498bc6f7bdec71b4258
soupeater-of-boreal-valley <121794132+soupeater-of-boreal-valley@users.noreply.github.com>
2023-10-11 17:12:24 +0100

    up-to-date HD mang.

--- VM/cldrv.py
+++ VM/cldrv.py
@@ -1,17 +1,2 @@
-
-def powr(ind, by):
-
-    out = ind
-    for l in range(by):
-        out *= ind
-    return out
-
-dat = 4294967296 / 2
-
-print( dat / 1073741824, 'Gigabytes')
-a = input()
-
-import math
-
-dat = '\x00' * math.floor(dat)
+dat = ''
 open('drives\\C.drv', 'w').write(dat)
